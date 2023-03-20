e663777ec3926a313f702cf2528ab8f8d1cff121
qa-obbun-0-datl@example.com <qa-obbun-0-datl@example.com>
2023-03-20 16:00:56 -0500

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjE0NDkwMw/spectral.js?branch_slug=main&token=08b4e388-ee83-480c-a70e-8c488754c398";
+export default { extends: ruleset };
